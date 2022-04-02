455e4481d58fdc91cfbd5408c91fd7b8c2fa4e94
Armando Capriles <armandotechno12@gmail.com>
2022-04-01 22:59:15 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f827682da9d612a0c7d75864aff59a83'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7b76006c-1b98-461a-aaa0-c2f70c1e1046'
+};
